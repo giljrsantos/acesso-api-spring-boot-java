e198d03ee6e5c13eddb3826f311434e3c099d6a0
Gilberto Junior <giljrsantos@gmail.com>
2026-06-18 19:18:11 -0300

    add findByEmail method to UsuarioRepository for retrieving UsuarioEntity by email

--- src/main/java/io/dev/acesso_api/adapter/repositories/UsuarioRepository.java
+++ src/main/java/io/dev/acesso_api/adapter/repositories/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package io.dev.acesso_api.adapter.repositories;
 
 import io.dev.acesso_api.adapter.entities.UsuarioEntity;
+import io.dev.acesso_api.core.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
+    UsuarioEntity findByEmail(String email);
 }
