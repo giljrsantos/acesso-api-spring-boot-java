6d0de332f0acdbae39a506384055c8a4adecb4bd
Gilberto Junior <giljrsantos@gmail.com>
2026-06-18 19:18:20 -0300

    add relationship to PessoaEntity in UsuarioEntity for user association

--- src/main/java/io/dev/acesso_api/adapter/entities/UsuarioEntity.java
+++ src/main/java/io/dev/acesso_api/adapter/entities/UsuarioEntity.java
@@ -1,20 +1,21 @@
 package io.dev.acesso_api.adapter.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Table(name = "tb_usuario")
 public class UsuarioEntity {
-
     @Id
     @GeneratedValue
     private Long id;
     private String email;
     private String senha;
     private Boolean administrador;
+    @OneToOne
+    @JoinColumn(name = "pessoa_id")
+    private PessoaEntity pessoaEntity;
 }
