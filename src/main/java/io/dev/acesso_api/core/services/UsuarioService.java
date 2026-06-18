cb3fd664aafa2ef09b81d6886d7baafd7e9d4596
Gilberto Junior <giljrsantos@gmail.com>
2026-06-18 08:54:41 -0300

    add UsuarioService implementation for user creation using repository port

--- src/main/java/io/dev/acesso_api/core/services/UsuarioService.java
+++ src/main/java/io/dev/acesso_api/core/services/UsuarioService.java
@@ -1,12 +1,20 @@
 package io.dev.acesso_api.core.services;
 
 import io.dev.acesso_api.core.domain.Usuario;
+import io.dev.acesso_api.core.ports.UsuarioRepositoryPort;
 import io.dev.acesso_api.core.ports.UsuarioServicePort;
 import org.springframework.stereotype.Service;
 
 public class UsuarioService implements UsuarioServicePort {
+
+    private final UsuarioRepositoryPort usuarioRepositoryPort;
+
+    public UsuarioService(UsuarioRepositoryPort usuarioRepositoryPort) {
+        this.usuarioRepositoryPort = usuarioRepositoryPort;
+    }
+
     @Override
     public Usuario createUsuario(Usuario usuario) {
-        return usuario;
+        return usuarioRepositoryPort.create(usuario);
     }
 }
