b6de948699f6ec90c2b6aac95c94417ff1bfef86
Gilberto Junior <giljrsantos@gmail.com>
2026-06-18 19:17:56 -0300

    add createPessoa method and obtainByEmail implementation in UsuarioRepositoryAdapter

--- src/main/java/io/dev/acesso_api/adapter/repositories/UsuarioRepositoryAdapter.java
+++ src/main/java/io/dev/acesso_api/adapter/repositories/UsuarioRepositoryAdapter.java
@@ -1,6 +1,8 @@
 package io.dev.acesso_api.adapter.repositories;
 
+import io.dev.acesso_api.adapter.entities.PessoaEntity;
 import io.dev.acesso_api.adapter.entities.UsuarioEntity;
+import io.dev.acesso_api.core.domain.Pessoa;
 import io.dev.acesso_api.core.domain.Usuario;
 import io.dev.acesso_api.core.ports.UsuarioRepositoryPort;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,28 @@ import org.springframework.stereotype.Component;
 public class UsuarioRepositoryAdapter implements UsuarioRepositoryPort {
 
     private final UsuarioRepository usuarioRepository;
+    private final PessoaRepository pessoaRepository;
     private final ModelMapper modelMapper;
 
     @Override
     public Usuario create(Usuario usuario) {
-        UsuarioEntity novoUsuario =  usuarioRepository.save(modelMapper.map(usuario, UsuarioEntity.class));
+        UsuarioEntity usuarioEntity = modelMapper.map(usuario, UsuarioEntity.class);
+        usuarioEntity.setPessoaEntity(createPessoa(usuario.getPessoa()));
+        UsuarioEntity novoUsuario =  usuarioRepository.save(usuarioEntity);
         return modelMapper.map(novoUsuario, Usuario.class);
+    }
+
+    @Override
+    public Usuario obtainByEmail(String email) {
+        UsuarioEntity usuarioByEmail = usuarioRepository.findByEmail(email);
+        if(usuarioByEmail == null){
+            return null;
+        }
+        return  modelMapper.map(usuarioByEmail, Usuario.class);
+    }
+
+    private PessoaEntity createPessoa(Pessoa pessoa){
+        PessoaEntity pessoaEntity = modelMapper.map(pessoa, PessoaEntity.class);
+        return pessoaRepository.save(pessoaEntity);
     }
 }
