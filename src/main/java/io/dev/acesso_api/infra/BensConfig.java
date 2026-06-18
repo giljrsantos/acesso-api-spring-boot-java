0509ed929e3bf5519fc7e1c5f18b79e520606284
Gilberto Junior <giljrsantos@gmail.com>
2026-06-18 08:55:10 -0300

    add ModelMapper bean and update UsuarioService implementation to use UsuarioRepositoryPort

--- src/main/java/io/dev/acesso_api/infra/BensConfig.java
+++ src/main/java/io/dev/acesso_api/infra/BensConfig.java
@@ -1,7 +1,9 @@
 package io.dev.acesso_api.infra;
 
+import io.dev.acesso_api.core.ports.UsuarioRepositoryPort;
 import io.dev.acesso_api.core.ports.UsuarioServicePort;
 import io.dev.acesso_api.core.services.UsuarioService;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class BensConfig {
 
     @Bean
-    public UsuarioServicePort usuarioServiceImpl(){
-        return new UsuarioService();
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public UsuarioServicePort usuarioServiceImpl(UsuarioRepositoryPort usuarioRepositoryPort){
+        return new UsuarioService(usuarioRepositoryPort);
     }
 }
