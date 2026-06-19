6c2a009a6d2c1d3f2e7f608f5ed1c95a021e4016
Gilberto Junior <giljrsantos@gmail.com>
2026-06-19 12:15:40 -0300

    add MoradorEntity class with JPA annotations and relationships

---
+++ src/main/java/io/dev/acesso_api/adapter/entities/MoradorEntity.java
@@ -0,0 +1,23 @@
+package io.dev.acesso_api.adapter.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "tb_morador")
+public class MoradorEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String cpf;
+    private String endereco;
+    private String celular;
+    @OneToOne
+    @JoinColumn(name = "pessoa_id")
+    private PessoaEntity pessoaEntity;
+}
