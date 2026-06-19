6125c35c90f1c8a7757257b4bbd5e2ddd39c7472
Gilberto Junior <giljrsantos@gmail.com>
2026-06-19 12:14:09 -0300

    add PessoaRepositoryAdapter for creating PessoaEntity with ModelMapper integration

---
+++ src/main/java/io/dev/acesso_api/adapter/repositories/PessoaRepositoryAdapter.java
@@ -0,0 +1,20 @@
+package io.dev.acesso_api.adapter.repositories;
+
+import io.dev.acesso_api.adapter.entities.PessoaEntity;
+import io.dev.acesso_api.core.domain.Pessoa;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class PessoaRepositoryAdapter {
+
+    private final PessoaRepository pessoaRepository;
+    private final ModelMapper modelMapper;
+
+    public PessoaEntity createPessoa(Pessoa pessoa){
+        PessoaEntity pessoaEntity = modelMapper.map(pessoa, PessoaEntity.class);
+        return pessoaRepository.save(pessoaEntity);
+    }
+}
