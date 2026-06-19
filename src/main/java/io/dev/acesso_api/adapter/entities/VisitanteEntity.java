563be895a0d078dae263df10e42eb1aff40149e8
Gilberto Junior <giljrsantos@gmail.com>
2026-06-19 18:41:19 -0300

    add VisitanteEntity class for Visitante data representation

---
+++ src/main/java/io/dev/acesso_api/adapter/entities/VisitanteEntity.java
@@ -0,0 +1,21 @@
+package io.dev.acesso_api.adapter.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "tb_visitante")
+public class VisitanteEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String rg;
+    @OneToOne
+    @JoinColumn(name = "pessoa_id")
+    private PessoaEntity pessoaEntity;
+}
