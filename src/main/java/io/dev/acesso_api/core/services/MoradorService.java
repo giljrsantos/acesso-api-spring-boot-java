417f9f046beddead0c1316e00a4bf9fece583188
Gilberto Junior <giljrsantos@gmail.com>
2026-06-19 18:45:59 -0300

    refactor MoradorService to use Morador domain object instead of MoradorEntity

--- src/main/java/io/dev/acesso_api/core/services/MoradorService.java
+++ src/main/java/io/dev/acesso_api/core/services/MoradorService.java
@@ -1,6 +1,6 @@
 package io.dev.acesso_api.core.services;
 
-import io.dev.acesso_api.adapter.entities.MoradorEntity;
+
 import io.dev.acesso_api.core.domain.Morador;
 import io.dev.acesso_api.core.ports.MoradorRepositoryPort;
 import io.dev.acesso_api.core.ports.MoradorServicePort;
