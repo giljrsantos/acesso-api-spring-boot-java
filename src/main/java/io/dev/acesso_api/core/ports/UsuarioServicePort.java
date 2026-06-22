19cff80ed2f9492afa072589be12749abcfb2aa4
Gilberto Junior <giljrsantos@gmail.com>
2026-06-22 20:28:17 -0300

    add methods to retrieve all users and get user by email in UsuarioServicePort

--- src/main/java/io/dev/acesso_api/core/ports/UsuarioServicePort.java
+++ src/main/java/io/dev/acesso_api/core/ports/UsuarioServicePort.java
@@ -2,7 +2,13 @@ package io.dev.acesso_api.core.ports;
 
 import io.dev.acesso_api.core.domain.Usuario;
 
+import java.util.Collection;
+
 public interface UsuarioServicePort {
 
     Usuario createUsuario(Usuario usuario);
+
+    Collection<Usuario> getAllUsers();
+
+    Usuario getByEmail(String email);
 }
