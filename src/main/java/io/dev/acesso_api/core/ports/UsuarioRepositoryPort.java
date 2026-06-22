3e66badb66559306a7c50571f85c7b0e41f3535b
Gilberto Junior <giljrsantos@gmail.com>
2026-06-22 20:27:33 -0300

    refactor UsuarioRepositoryPort to return Optional<Usuario> for obtainByEmail method and add getAllUsers method

--- src/main/java/io/dev/acesso_api/core/ports/UsuarioRepositoryPort.java
+++ src/main/java/io/dev/acesso_api/core/ports/UsuarioRepositoryPort.java
@@ -2,7 +2,11 @@ package io.dev.acesso_api.core.ports;
 
 import io.dev.acesso_api.core.domain.Usuario;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface UsuarioRepositoryPort {
     public Usuario create(Usuario usuario);
-    public Usuario obtainByEmail(String email);
+    public Optional<Usuario> obtainByEmail(String email);
+    public Collection<Usuario> getAllUsers();
 }
