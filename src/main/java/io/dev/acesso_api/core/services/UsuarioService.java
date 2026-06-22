37748590ebaec76c98aef6d1f845b31fa935b20a
Gilberto Junior <giljrsantos@gmail.com>
2026-06-22 20:28:03 -0300

    refactor UsuarioService to improve user retrieval and existence checks

--- src/main/java/io/dev/acesso_api/core/services/UsuarioService.java
+++ src/main/java/io/dev/acesso_api/core/services/UsuarioService.java
@@ -2,13 +2,18 @@ package io.dev.acesso_api.core.services;
 
 import io.dev.acesso_api.core.domain.Usuario;
 import io.dev.acesso_api.core.exception.BusinessException;
+import io.dev.acesso_api.core.exception.BusinessNotFoundException;
 import io.dev.acesso_api.core.ports.UsuarioRepositoryPort;
 import io.dev.acesso_api.core.ports.UsuarioServicePort;
+
+import java.util.Collection;
+import java.util.List;
 
 
 public class UsuarioService implements UsuarioServicePort {
 
     private final UsuarioRepositoryPort usuarioRepositoryPort;
+
 
     public UsuarioService(UsuarioRepositoryPort usuarioRepositoryPort) {
         this.usuarioRepositoryPort = usuarioRepositoryPort;
@@ -16,10 +21,21 @@ public class UsuarioService implements UsuarioServicePort {
 
     @Override
     public Usuario createUsuario(Usuario usuario) {
-        Usuario usuarioExistente = usuarioRepositoryPort.obtainByEmail(usuario.getEmail());
-        if(usuarioExistente != null){
-            throw new BusinessException("Usuário já existe com esse email");
-        }
+        usuarioRepositoryPort.obtainByEmail(usuario.getEmail())
+                .ifPresent(u -> {
+                    throw new BusinessException("Usuário já existe com esse email");
+                });
         return usuarioRepositoryPort.create(usuario);
+    }
+
+    @Override
+    public Usuario getByEmail(String email) {
+        return usuarioRepositoryPort.obtainByEmail(email)
+                .orElseThrow(() -> new BusinessNotFoundException("Usuário não encontrado com esse email!"));
+    }
+
+    @Override
+    public Collection<Usuario> getAllUsers() {
+        return usuarioRepositoryPort.getAllUsers();
     }
 }
